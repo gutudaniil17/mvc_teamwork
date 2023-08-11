3d4cf6c593d98cf939c46c36df92058b7a7cb80d
gutudaniil17 <daniil.gutu@iongroup.com>
2023-08-11 12:47:03 +0300

    Deleted permissions

--- src/main/java/com/example/mvc_teamwork/security/entity/user/Role.java
+++ src/main/java/com/example/mvc_teamwork/security/entity/user/Role.java
@@ -13,33 +13,8 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public enum Role {
 
-    USER(
-            Set.of(
-                    Permission.ADMIN_READ,
-                    Permission.ADMIN_UPDATE,
-                    Permission.ADMIN_DELETE,
-                    Permission.ADMIN_CREATE
-            )
-    ),
+    USER,
 
-    ADMIN(
-            Set.of(
-                    Permission.ADMIN_READ,
-                    Permission.ADMIN_UPDATE,
-                    Permission.ADMIN_DELETE,
-                    Permission.ADMIN_CREATE
-            )
-    );
+    ADMIN;
 
-    @Getter
-    private final Set<Permission> permissions;
-
-    public List<SimpleGrantedAuthority> getAuthorities(){
-        var authorities = getPermissions()
-                .stream()
-                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
-                .collect(Collectors.toList());
-        authorities.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
-        return authorities;
-    }
 }
