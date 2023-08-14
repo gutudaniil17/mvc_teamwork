cf4023c4fd0aa666cb0b64d9cb75169986b2e3f0
gutudaniil17 <daniil.gutu@iongroup.com>
2023-08-14 11:46:41 +0300

    Create service and controller for categories

--- src/main/java/com/example/mvc_teamwork/repository/CategoryRepository.java
+++ src/main/java/com/example/mvc_teamwork/repository/CategoryRepository.java
@@ -1,11 +1,17 @@
 package com.example.mvc_teamwork.repository;
 
 import com.example.mvc_teamwork.entity.Category;
+import com.example.mvc_teamwork.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category,Integer> {
+    Category save(Category category);
+    Category findByName(String name);
+    void deleteById(int id);
+    List<Category> findAll();
 }
