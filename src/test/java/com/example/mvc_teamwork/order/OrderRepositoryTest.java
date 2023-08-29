bd269e725129b96071aa8a5aaee69fcca394d4dd
ericadiaciuc <erica.diaciuc@iongroup.com>
2023-08-29 13:39:41 +0300

    added tests gor order and shoppingCart

---
+++ src/test/java/com/example/mvc_teamwork/order/OrderRepositoryTest.java
@@ -0,0 +1,2 @@
+package com.example.mvc_teamwork.order;public class OrderRepositoryTest {
+}
---
+++ src/test/java/com/example/mvc_teamwork/shoppingCart/ShoppingCartRepositoryTest.java
@@ -0,0 +1,2 @@
+package com.example.mvc_teamwork.shoppingCart;public class ShoppingCartRepositoryTest {
+}
